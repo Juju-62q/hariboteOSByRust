64e38529e54af67688c183f691c286a0866abcf4
tamarindsoup <san.jose3993@gmail.com>
2018-03-23 00:57:58 +0900

    docker-compose.ymlの追加

--- boot.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker build -t test .
-docker run -ti --rm \
-       -e DISPLAY=${DISPLAY} \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v ${PWD}:/home/developer/haribote\
-       test /bin/bash
